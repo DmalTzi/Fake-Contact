eb3154382f6a402766dec932fdd3f7db8ec3dac4
DmalTzi <audyomg@gmail.com>
2026-04-02 22:20:28 +0700

    3th

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script>
-  let title = "สอนทำ Svelte SEO ให้ติดหน้าแรก";
-  let description = "คู่มือฉบับสมบูรณ์สำหรับการปรับแต่ง SvelteKit ให้เป็นมิตรกับ Google";
+    import image from "$lib/assets/profile.jpg"
+    let title = "(@dmaltzi) • Instagram profile";
+    let description = "435 Followers, 3,267 Following, 120 Posts - See Instagram photos and videos from (@dmaltzi)";
+    let url = "https://instagram.dmaltzi.com/"
 </script>
 
 <svelte:head>
-  <title>{title}</title>
-  <meta name="description" content={description} />
-  
-  <meta property="og:title" content={title} />
-  <meta property="og:description" content={description} />
-  <meta property="og:type" content="website" />
+    <title>{title}</title>
+    <meta name="title" content={title}>
+    <meta name="description" content={description} />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={url} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={image}/>  
+    <meta property="og:type" content="website" />
 </svelte:head>
 
 <h1>Welcome to SvelteKit</h1>
