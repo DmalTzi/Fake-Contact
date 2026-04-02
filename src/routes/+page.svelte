572b158e421ead8c42fdfff950007027bb25c795
DmalTzi <audyomg@gmail.com>
2026-04-02 22:36:27 +0700

    7th

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script>
     import image from "$lib/assets/profile.jpg"
+    import { onMount } from "svelte";
     let title = "(@dmaltzi) • Instagram profile";
     let description = "435 Followers, 3,267 Following, 120 Posts - See Instagram photos and videos from (@dmaltzi)";
-    let url = "https://instagram.dmaltzi.com/"
+    let rickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    let url = "https://instagram.dmaltzi.com"
     let imageUrl = url + image;
+    onMount(() => {
+        setTimeout(() => {
+            window.location.href = rickroll;
+        }, 500);
+    })
 </script>
 
 <svelte:head>
@@ -14,9 +21,14 @@
     <meta property="og:url" content={url} />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
-    <meta property="og:image" content={imageUrl}/>  
-    <meta property="og:type" content="website" />
+    <meta property="og:image" content={imageUrl} />
+    
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content={imageUrl} />
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div style="font-family: sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <p>Redirecting to Instagram...</p>
+</div>
